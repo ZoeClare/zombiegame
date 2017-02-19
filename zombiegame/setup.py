293eaadd20776b08369cce3b407f3a50905a645e
Zoe Cook <cookz1995@gmail.com>
2017-02-19 17:51:35 +0000

    initialise repository

---
+++ zombiegame/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Zombie Game',
+    version='0.0.0000001',
+    author='Zoe Cook',
+    author_email='cookz1995@gmail.com',
+    packages=['zombiegame'],
+    license='LICENSE.txt',
+    description='Kill the zombies D: oh noez',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
